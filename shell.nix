fbf322f15c928f9960c2b64f5a3b1adb6efe64d1
Miguel Villa Floran <miguel.villafloran@gmail.com>
2026-02-23 00:42:03 -0800

    Update Nix shell

--- shell.nix
+++ shell.nix
@@ -2,21 +2,24 @@
   pkgs ? import <nixpkgs> { config.allowUnfree = true; },
 }:
 
+let
+  python = pkgs.python312;
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    python311 # Python 3.11
-    uv # Python package manager
-    nixfmt # Nix formatter
-    just # Just runner
+    python
+    uv
+    nixfmt
+    just
     cyclonedds
   ];
 
   shellHook = ''
-    export CYCLONEDDS_HOME=${pkgs.cyclonedds}
-    export CMAKE_PREFIX_PATH="$CYCLONEDDS_HOME:$CMAKE_PREFIX_PATH"
-    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.zlib}/lib:${pkgs.wayland}/lib:${pkgs.libxkbcommon}/lib:${pkgs.libGL}/lib:${pkgs.libglvnd}/lib:${pkgs.mesa}/lib:${pkgs.xorg.libX11}/lib:${pkgs.xorg.libXcursor}/lib:${pkgs.xorg.libXrandr}/lib:${pkgs.xorg.libXi}/lib:$CYCLONEDDS_HOME/lib:$LD_LIBRARY_PATH"
-    export CFLAGS="-I$CYCLONEDDS_HOME/include/idlc $CFLAGS"
     export TMPDIR=/tmp
+    export UV_PYTHON="${python}/bin/python"
+    export CYCLONEDDS_HOME="${pkgs.cyclonedds}"
+    export CMAKE_PREFIX_PATH="${pkgs.cyclonedds}:$CMAKE_PREFIX_PATH"
+    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.zlib}/lib:${pkgs.cyclonedds}/lib:$LD_LIBRARY_PATH"
     just install
   '';
 }
